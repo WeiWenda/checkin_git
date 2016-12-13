b1f13d81a1cb3e500fbf0c6c0dfd0f1dc54ddd32
weiwenda <weiwenda@stu.xjtu.edu.cn>
2016-12-13 15:43:44 +0800

    first commit

---
+++ checkin.py
@@ -0,0 +1,57 @@
+#coding:utf-8
+# all the imports
+import MySQLdb as mysql
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash,jsonify, send_from_directory
+import datetime
+from checkin_related import checkin_related
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# configuration
+DATABASE = 'checkin'
+DEBUG = True
+HOST='202.117.16.35'
+SECRET_KEY = 'development key'
+USERNAME = 'vivid'
+PASSWORD = 'root'
+
+app = Flask(__name__,static_folder = 'static')
+app.config.from_object(__name__)
+
+
+# Register Pages
+app.register_blueprint(checkin_related)
+
+def connect_db():
+    return mysql.connect(host=app.config['HOST'],user=app.config['USERNAME'],\
+        passwd=app.config['PASSWORD'],db=app.config['DATABASE'],charset='utf8')
+
+def get_cursor():
+    return g.db.cursor()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+    g.cursor = get_cursor()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+        g.cursor.close()
+
+
+# def add_entry():
+#     if not session.get('logged_in'):
+#         abort(401)
+#     g.cursor.execute('insert into blog (title, content) values (?, ?)',
+#                  [request.form['title'], request.form['content']])
+#     g.db.commit()
+#     flash('New content was successfully posted')
+#     return redirect(url_for('show_entries'))
+   
+if __name__ == '__main__':
+	app.run(host='202.117.16.35')
---
+++ checkin_related.py
@@ -0,0 +1,68 @@
+# -*- coding: utf8 -*-
+import os
+from flask import Blueprint, render_template, abort,Flask, request, session, g, redirect, url_for,flash,jsonify,request 
+from datetime import datetime
+import MySQLdb as mysql
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+checkin_related = Blueprint("checkin_related", __name__, template_folder="templates")
+
+@checkin_related.route('/')
+def Checkin():
+    if request.method=='GET':
+        g.cursor.execute('select group_id,group_name from lab_group order by group_id')
+        entries = [dict(id=row[0], name=row[1]) for row in g.cursor.fetchall()]
+        return render_template('Checkin.html',entries=entries)
+
+    
+@checkin_related.route('/stu_list',methods=['POST','GET'])
+def Checkin_Stu_List():
+    groupid=int(request.form['id'])
+    g.cursor.execute('select stu_id,stu_name,bg_color from lab_student,lab_grade where group_id = %d  and lab_student.grade_id = lab_grade.grade_id order by lab_student.grade_id '% groupid)
+    entries = [dict(id=row[0], name=row[1],color=row[2]) for row in g.cursor.fetchall()]
+    return jsonify(entries)
+
+@checkin_related.route('/stu_detail',methods=['GET','POST'])
+def Checkin_Stu_Detail():
+    start = request.args.get('start')
+    end = request.args.get('end')
+    stu_id = int(request.args.get('stu_id'))
+    g.cursor.execute('select start,color from lab_event where  stu_id = %s and start between %s and %s ',(stu_id,start,end))
+    ret = [dict(start=row[0].isoformat(" "),backgroundColor=row[1],borderColor=row[1]) for row in g.cursor.fetchall()]
+    return jsonify(ret)
+
+@checkin_related.route('/count_detail',methods=['GET','POST'])
+def Checkin_Count_Detail():
+    start = request.form['ds']
+    end = request.form['de']
+    g.cursor.execute('select lab_group.group_id,ifnull(count_exist,0) from (select lab_group.group_id,avg(count_sum) as count_exist from (select stu_id,sum(sign_count) as count_sum from lab_sign_count where sign_date between %s and %s group by stu_id ) as stu_count,lab_group,lab_student where stu_count.stu_id = lab_student.stu_id AND lab_student.group_id = lab_group.group_id group by lab_group.group_id ) as group_count right OUTER JOIN lab_group on group_count.group_id = lab_group.group_id order by lab_group.group_id',(start,end))
+    group_count=[int(row[1]) for row in g.cursor.fetchall()]
+    g.cursor.execute('select group_id,group_name from lab_group order by group_id')
+    group_name = [row[1][:-2] for row in g.cursor.fetchall()]
+    ret = dict(row=group_name,col=group_count)
+    return jsonify(ret)
+
+@checkin_related.route('/count_group',methods=['GET','POST'])
+def Checkin_Count_Group():
+    group_id = int( request.args.get('id'))
+    start = request.form['ds']
+    end = request.form['de']
+    g.cursor.execute('select s.stu_id,s.stu_name,sum(sign_count) as count_sum from lab_sign_count as c,lab_student as s,lab_group as g where c.sign_date between "%s" and  "%s" and c.stu_id = s.stu_id and s.group_id = g.group_id and g.group_id = %d group by s.stu_id,s.stu_name order by s.grade_id'%(start,end,group_id))
+    result=g.cursor.fetchall()
+    group_count=[int(row[2]) for row in result]
+    group_name = [row[1] for row in result]
+    ret = dict(row=group_name,col=group_count)
+    return jsonify(ret)
+
+@checkin_related.route('/count_stu',methods=['GET','POST'])
+def Checkin_Count_Student():
+    student_id = int( request.args.get('id'))
+    start = request.form['ds']
+    end = request.form['de']
+    g.cursor.execute('select sign_date,sign_count from lab_sign_count  where stu_id = %d  and sign_date between "%s" and "%s" order by sign_date'%(student_id,start,end))
+    result=g.cursor.fetchall()
+    group_count=[int(row[1]) for row in result]
+    group_name = [row[0].strftime('%m-%d') for row in result]
+    ret = dict(row=group_name,col=group_count)
+    return jsonify(ret)
